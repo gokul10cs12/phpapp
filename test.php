a358adadcbd62f189ca781144abe0a1036ee3009
gokul <gokul10cs@gmail.com>
2019-01-28 00:14:19 +0530

    app

---
+++ test.php
@@ -0,0 +1,13 @@
+<html>
+<body bgcolor="Grey">
+	<center>
+<h1>Hi There!</h1>	
+<form action="welcome.php" method="post">
+	
+	
+
+<input type="submit" value="Click Me" name="Save">
+</form>
+</center>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body bgcolor="grey">
+	<center>
+		<h1>
+<?php
+
+	
+
+	echo " THANKS FOR THE VISIT";
+
+
+?>
+<br>
+</h1>
+<a href="test.php">click here to return home </a>
+
+</center>
+</body>
+</html>
